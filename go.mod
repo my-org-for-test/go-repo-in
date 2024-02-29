ad58483a874bba62e7d436376a1c33a383b53327
TarasHrynchuk <125279218+TarasHrynchuk@users.noreply.github.com>
2024-02-29 17:50:41 +0100

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/my-org-for-test/go-repo-in
+
+go 1.22
+
+require github.com/my-org-for-test/go-repo-out v1.0.1
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/my-org-for-test/go-repo-out/dependency"
+)
+
+func main() {
+	fmt.Println("Dependency Demo:")
+	dependency.PrintDependencyMessage()
+}
